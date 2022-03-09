89bc357c5a81594c859b9e64fb42670dc076a232
Onkar Birajdar <onkarabirajdar@gmail.com>
2022-03-10 03:28:28 +0530

    Setup app.js along with required pakages

---
+++ .d.ts
@@ -0,0 +1,4 @@
+declare module 'ejs';
+declare module 'express';
+declare module 'body-parser';
+declare module 'mongoose';
--- app.js
+++ app.js
@@ -8,3 +8,43 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const wikiSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+const Wiki = mongoose.model("wiki", wikiSchema);
+
+const homeStartingContent = new Wiki({
+    title: "Welcome to the Wiki",
+    content:
+        "This is your home page. You can edit this page by clicking on the edit button.",
+});
+
+let wikiList = [homeStartingContent];
+
+Wiki.insertMany(wikiList, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Wiki list added");
+    }
+});
+
+Wiki.bulkSave();
+
+app.get("/", function (req, res) {
+    res.send(
+        Wiki.findOne({ title: "Welcome to the Wiki" }).then(function (result) {
+            res.render("home", {
+                //rendering the home page
+                homeStartingContent: result,
+            });
+        })
+    );
+});
