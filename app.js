e086b3acceb60a374acdbb4ae974655c3a0593a8
Onkar Birajdar <onkarabirajdar@gmail.com>
2022-03-11 21:21:06 +0530

    Inital commit

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
     useNewUrlParser: true,
