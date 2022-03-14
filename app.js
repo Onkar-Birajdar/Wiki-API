50ce9004a2b02e3b2f920c2ebe2249b91826a81b
Onkar Birajdar <onkarabirajdar@gmail.com>
2022-03-14 23:26:39 +0530

    setting up rout parameters

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
@@ -30,25 +29,73 @@ const homeStartingContent = new Wiki({
 
 let wikiList = [homeStartingContent];
 
-Wiki.insertMany(wikiList, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Wiki list added");
-    }
-});
+app.route("/articles")
+    .get((req, res) => {
+        Wiki.find({}, function (err, foundWiki) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundWiki.length === 0) {
+                    Wiki.insertMany(wikiList, function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Wiki list added");
+                        }
+                    });
+                    res.redirect("/");
+                } else {
+                    res.render("home", { wikiList: foundWiki });
+                }
+            }
+        }
+            .post("/articles", function (req, res) {
+                var title = req.body.title;
+                var content = req.body.content;
+                const temp = new Wiki({
+                    title: title,
+                    content: content,
+                });
+                temp.save();
+                res.send("I got the response as " + title + " " + content);
+    
+            }
+                .delete("/articles", function (req, res) {
+                    Wiki.deleteMany({}, function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Wiki list deleted");
+                        }
+                    });
+                    res.redirect("/");
+                }
+     });
 
 app.get("/", function (req, res) {
-    res.send(
-        // Wiki.findOne({ title: "Welcome to the Wiki" }).then(function (result) {
-        //     // res.render("home", {
-        //     //     //rendering the home page
-        //     //     homeStartingContent: result,
-        //     // });
-        // })
-        "Hello World"
-    );
+    
+    Wiki.find({}, function (err, foundWiki) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundWiki.length === 0) {
+                Wiki.insertMany(wikiList, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Wiki list added");
+                    }
+                });
+                res.redirect("/");
+            } else {
+                res.render("home", { wikiList: foundWiki });
+            }
+        }
+    });
+    
 });
+
+app.
 
 app.listen(port, () => {
     console.log("Server started on port 3000");
